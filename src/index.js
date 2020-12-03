0b76825fe1187f1c78af8b8c272073306207c0d1
Julio <julio.chazari@gmail.com>
2020-12-03 17:59:25 -0600

    Wrapped App with CookiesProvider

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { CookiesProvider } from 'react-cookie';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from './theme';
@@ -7,10 +8,12 @@ import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <Router>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  </Router>,
+  <CookiesProvider>
+    <Router>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </Router>
+  </CookiesProvider>,
   document.getElementById('root')
 );
