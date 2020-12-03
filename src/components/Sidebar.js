e2a2c946f0403cb2f8c8f43817e314cd38739916
Julio <julio.chazari@gmail.com>
2020-12-03 16:35:16 -0600

    Change Link to NavLink to achieve 'active link' depending on which route I am on

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import { ReactComponent as Logo } from '../napm-logo-light.svg';
 import { RiMenuLine, RiSettings3Fill, RiLogoutBoxRFill } from 'react-icons/ri';
 import { MdDashboard } from 'react-icons/md';
@@ -23,24 +24,24 @@ function Sidebar() {
         <Logo className="logo item-text" />
       </div>
       <div className="sidebar-items">
-        <a href="#home" className="item">
+        <NavLink to="/dashboard" className="item" activeClassName="dash-active" exact>
           <MdDashboard className="sidebar-icon" />
           <span className="item-text">Home</span>
-        </a>
-        <a href="#Insights" className="item">
+        </NavLink>
+        <NavLink to="/dashboard/insights" className="item" activeClassName="dash-active">
           <IoMdStats className="sidebar-icon" />
           <span className="item-text">Insights</span>
-        </a>
-        <a href="#inbox" className="item">
+        </NavLink>
+        <NavLink to="/dashboard/inbox" className="item" activeClassName="dash-active">
           <HiBell className="sidebar-icon" />
           <span className="item-text">Inbox</span>
-        </a>
+        </NavLink>
       </div>
       <div className="dashboard-controls sidebar-items">
-        <a href="#settings" className="item">
+        <NavLink to="/dashboard/settings" className="item" activeClassName="dash-active">
           <RiSettings3Fill className="sidebar-icon" />
           <span className="item-text">Settings</span>
-        </a>
+        </NavLink>
         <div className="item logout">
           <RiLogoutBoxRFill className="sidebar-icon" />
           <span className="item-text">Log out</span>
