348b9e50f22feaa575f7dc6e1ddac192ab1c13d5
Julio <julio.chazari@gmail.com>
2020-12-03 16:31:36 -0600

    Cleaned a line or two

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from './theme';
+import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </Router>
-  </React.StrictMode>,
+  <Router>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </Router>,
   document.getElementById('root')
 );
