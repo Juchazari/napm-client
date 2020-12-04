81d0d9f5de1d01a3e8135e122cf82328b7d05adc
Julio <julio.chazari@gmail.com>
2020-12-03 18:27:41 -0600

    Removed CooliesProvider wrapper

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { CookiesProvider } from 'react-cookie';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from './theme';
@@ -8,12 +7,10 @@ import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <CookiesProvider>
-    <Router>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </Router>
-  </CookiesProvider>,
+  <Router>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </Router>,
   document.getElementById('root')
 );
