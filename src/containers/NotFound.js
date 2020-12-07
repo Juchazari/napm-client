037ed08eb72992aa08a8f301477dd85465bf9547
Julio <julio.chazari@gmail.com>
2020-12-07 05:04:52 -0600

    Created Not found container for 404 errors

---
+++ src/containers/NotFound.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../napm-logo.svg';
+import './NotFound.css';
+
+function NotFound() {
+  return (
+    <div className="NotFound">
+      <Link to="/" style={{marginBottom: '16px'}}>
+        <Logo style={{width: '150px'}} />
+      </Link>
+      <h1>Sorry, that page doesn't exist!</h1>
+    </div>
+  );
+}
+
+export default NotFound;
