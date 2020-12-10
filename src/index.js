1806d5461d22ae95d2ef14a77b1849dd12127929
Julio <julio.chazari@gmail.com>
2020-12-09 23:19:10 -0600

    removed material-ui

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { ThemeProvider } from '@material-ui/core/styles';
-import theme from './theme';
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
   <Router>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </Router>,
   document.getElementById('root')
 );
