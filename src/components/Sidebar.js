acd58fd697321614127a7698917b85f35a520de8
Julio <julio.chazari@gmail.com>
2020-12-09 22:51:32 -0600

    Updated classnames for better readability

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { NavLink, useRouteMatch } from 'react-router-dom';
 import {
-  RiMenuLine,
-  RiDashboardFill,
-  RiSettings3Fill,
-  RiLogoutBoxRFill,
-  RiPieChartFill,
-  RiNotification3Fill
+  RiMenuLine as MenuIcon,
+  RiDashboardFill as DashboardIcon,
+  RiPieChartFill as ChartIcon,
+  RiNotification3Fill as BellIcon,
+  RiSettings3Fill as SettingsIcon,
+  RiLogoutBoxRFill as LogoutIcon
 } from 'react-icons/ri';
 import { ReactComponent as Logo } from '../napm-logo-light.svg';
 import ReactTooltip from 'react-tooltip';
@@ -27,35 +27,35 @@ function Sidebar({isExpanded, setIsExpanded}) {
 
   return (
     <div className={isExpanded ? "Sidebar" : "Sidebar collapsed"}>
-      <div className="sidebar-header">
-        <RiMenuLine onClick={handleToggler} className="sidebar-icon" />
-        <Logo className="logo item-text" />
+      <div className="sidebarHeader">
+        <MenuIcon onClick={handleToggler} className="navIcon" />
+        <Logo className="logo" />
       </div>
-      <div className="sidebar-items">
-        <NavLink to={`${url}`} data-tip="Home" className="item" activeClassName="dash-active" exact>
-          <RiDashboardFill className="sidebar-icon" />
-          <span className="item-text">Home</span>
+      <div className="sidebarNavLinkItems">
+        <NavLink to={`${url}`} data-tip="Home" className="navItem" activeClassName="navItemActive" exact>
+          <DashboardIcon className="navIcon" />
+          <span className="navText">Home</span>
         </NavLink>
-        <NavLink to={`${url}/insights`} data-tip="Insights" className="item" activeClassName="dash-active">
-          <RiPieChartFill className="sidebar-icon" />
-          <span className="item-text">Insights</span>
+        <NavLink to={`${url}/insights`} data-tip="Insights" className="navItem" activeClassName="navItemActive">
+          <ChartIcon className="navIcon" />
+          <span className="navText">Insights</span>
         </NavLink>
-        <NavLink to={`${url}/inbox`} data-tip="Inbox" className="item" activeClassName="dash-active">
-          <RiNotification3Fill className="sidebar-icon" />
-          <span className="item-text">Inbox</span>
+        <NavLink to={`${url}/inbox`} data-tip="Inbox" className="navItem" activeClassName="navItemActive">
+          <BellIcon className="navIcon" />
+          <span className="navText">Inbox</span>
         </NavLink>
       </div>
-      <div className="sidebar-controls sidebar-items">
-        <NavLink to={`${url}/settings`} data-tip="Settings" className="item" activeClassName="dash-active">
-          <RiSettings3Fill className="sidebar-icon" />
-          <span className="item-text">Settings</span>
+      <div className="sidebarControls">
+        <NavLink to={`${url}/settings`} data-tip="Settings" className="navItem" activeClassName="navItemActive">
+          <SettingsIcon className="navIcon" />
+          <span className="navText">Settings</span>
         </NavLink>
-        <div data-tip="Log out" className="item logout">
-          <RiLogoutBoxRFill className="sidebar-icon" />
-          <span className="item-text">Log out</span>
+        <div data-tip="Log out" className="navItem">
+          <LogoutIcon className="navIcon" />
+          <span className="navText">Log out</span>
         </div>
       </div>
-      {!isExpanded ? <ReactTooltip effect="solid" className="sidebar-tooltips" place="right" /> : null}
+      {!isExpanded && <ReactTooltip effect="solid" className="sidebar-tooltips" place="right" />}
     </div>
   );
 }
