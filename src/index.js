df8186e4c9be8d5317d5d152c34c24cc72efd50e
Julio <julio.chazari@gmail.com>
2020-12-11 17:14:55 -0600

    Removed imported css file and added GlobalStyle with styled-components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createGlobalStyle } from 'styled-components';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import './index.css';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body {
+    font-size: 16px;
+    font-family: 'Roboto', sans-serif;
+    color: #333;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
 
 ReactDOM.render(
   <Router>
+    <GlobalStyle />
     <App />
   </Router>,
   document.getElementById('root')
