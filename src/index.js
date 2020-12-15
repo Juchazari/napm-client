4e28019116b12711f773440f7809b8652f00ac26
Julio Chazari <julio.chazari@gmail.com>
2020-12-14 21:02:59 -0600

    Seperate styles folder file for global styles index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createGlobalStyle } from 'styled-components';
-import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-
-const GlobalStyle = createGlobalStyle`
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-  body {
-    font-size: 16px;
-    font-family: 'Roboto', sans-serif;
-    color: #333;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
-`;
+import { BrowserRouter as Router } from 'react-router-dom';
+import { GlobalStyle } from './assets/styles';
 
 ReactDOM.render(
   <Router>
