ab5fc1011d92a274bbf82527b3317d3ca475d9c3
Julio <julio.chazari@gmail.com>
2021-01-25 18:57:01 -0600

    Main Global Styles

---
+++ src/reset.css.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+    appearance: none;
+    text-decoration: none;
+  }
+  body {
+    font-family: 'Roboto', sans-serif;
+    font-size: 16px;
+    color: #333;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    overscroll-behavior: none;
+  }
+`;
