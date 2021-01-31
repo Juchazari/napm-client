4965588b12634c42af04af9242c7291e9f06bd6a
Julio Chazari <julio.chazari@gmail.com>
2021-01-30 19:05:26 -0600

    Imported Icon component and removed manual icon imports

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
-import {
-  RiDashboardFill as DashboardIcon,
-  RiSettings3Fill as SettingsIcon,
-  RiLogoutBoxRFill as LogoutIcon
-} from 'react-icons/ri';
 import ReactTooltip from 'react-tooltip';
-
-// RiMenuLine
-// RiPieChartFill
-// RiNotification3Fill
+import Icon from './Icon';
 
 const StyledSidebar = styled.div`
   position: fixed;
@@ -20,43 +12,37 @@ const StyledSidebar = styled.div`
   flex-direction: column;
   height: 100%;
   padding-top: 16px;
-  background-color: #252525;
+  background-color: #121212;
   color: #ffffff;
-  overflow-x: hidden;
-  overflow-y: auto;
-
-  svg {
-    width: 1.5em;
-    height: 1.5em;
-  }
 `;
 
 const NavItem = styled(NavLink)`
   display: flex;
   align-items: center;
+  justify-content: center;
   border: none;
   padding: 16px 24px;
-  background-color: transparent;
+  background-color: inherit;
   color: inherit;
   cursor: pointer;
   user-select: none;
 
   &:hover, &.active {
-    background-color: #2c2c2c;
+    background-color: #1a1a1a;
   }
 
   &.active {
-    box-shadow: 3px 0px 0px inset #ffffff;
+    box-shadow: 3px 0px 0px inset #1A73E8;
   }
 `;
 
 const ToolTip = styled(ReactTooltip)`
   font-size: 12px !important;
   padding: 5px 10px !important;
-  background-color: #1e272e !important;
+  background-color: #252525 !important;
 
   &.place-right:after {
-    border-right-color: #1e272e !important;
+    border-right-color: #252525 !important;
     border-right-style: solid !important;
   }
 `;
@@ -65,13 +51,13 @@ function Sidebar() {
   return (
     <StyledSidebar>
       <NavItem to="/" data-tip="Home" activeClassName="active" exact>
-        <DashboardIcon />
+        <Icon type="dashboard" size="20" />
       </NavItem>
       <NavItem to="/settings" data-tip="Settings" activeClassName="active">
-        <SettingsIcon />
+        <Icon type="settings" size="20" />
       </NavItem>
       <NavItem as="button" data-tip="Log out">
-        <LogoutIcon />
+        <Icon type="logout" size="20" />
       </NavItem>
       <ToolTip effect="solid" place="right" />
     </StyledSidebar>
